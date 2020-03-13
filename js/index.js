241321e0beacecbcd2ca8483a1490982b1d46a73
Nathan Fanelli <lifeisallonebigjoke@protonmail.com>
2020-03-12 22:14:38 -0500

    Modal added.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// Variables
+// Modal Variables
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+// Display modal on page load
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+// Close modal when user clicks on X (span)
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close modal when user clicks anywhere outside of modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
